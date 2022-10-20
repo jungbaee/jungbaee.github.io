fc9b734c2251b300ee52ef8baed81644a66c4008
jungbaee <92912032+jungbaee@users.noreply.github.com>
2022-10-20 16:35:28 -0700

    Add files via upload

---
+++ Project_1.php
@@ -0,0 +1,89 @@
+<?php include 'database.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name ="viewport" content ="width=device-width, initial-scale=1.0">
+    <title>Inventory Search</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="Project_1.php">Inventory Search</a>
+            <a class="navbar-brand" href="Project_2.php">Order</a>
+            <a class="navbar-brand" href="Project_3.php">Order Summary</a>
+        </nav>
+
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
+            <div class="form-group">
+                <label for="productId">Product_ID</label>
+                <input type="productId" class="form-control" id="productId" name="productId" placeholder="Enter Product_ID ex) 10001">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            
+        </form>
+        <br>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Product_ID</th>
+                    <th scope="col">Supplier_ID</th>
+                    <th scope="col">Product_Name</th>
+                    <th scope="col">List_Price</th>
+                    <th scope="col">Wholesales_Price</th>
+                    <th scope="col">Qty_On_Hand</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php 
+                    $productId = $productIdErr = "";
+                    if (isset($_GET['productId'])) {
+                        if (empty($_GET['productId'])) {
+                            $productIdErr = 'Product_Id is required';
+                            echo $productIdErr;
+                        } else {
+                            $productId = intval($_GET["productId"]);
+                            echo "Success";
+                        }
+                    }
+
+                    $sql = "SELECT * FROM Product WHERE Product_ID = '".$productId."'";
+                    $result = mysqli_query($conn, $sql);
+
+                    if ($result->num_rows > 0) {
+                        while($row = mysqli_fetch_array($result)) {
+                            echo "<tr>".
+                                    "<th scope='row'>".$row['Product_ID']."</th>".
+                                        "<td>".$row['Supplier_ID']."</td>".
+                                        "<td>".$row['Product_Name']."</td>".
+                                        "<td>".$row['List_Price']."</td>".
+                                        "<td>".$row['Wholesale_Price']."</td>".
+                                        "<td>".$row['Qty_On_Hand']."</td>".
+                                "</tr>";
+                        }
+                      } else {
+                        echo "<tr>".
+                                    "<th scope='row'>N/A</th>".
+                                        "<td>N/A</td>".
+                                        "<td>N/A</td>".
+                                        "<td>N/A</td>".
+                                        "<td>N/A</td>".
+                                        "<td>N/A</td>".
+                                "</tr>";
+                      }
+                      $conn->close();
+                      ?>
+            </tbody>
+        </table>
+        
+    </div>
+</body>
+
+</html>
+
---
+++ Project_2.php
@@ -0,0 +1,91 @@
+<?php include 'database.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name ="viewport" content ="width=device-width, initial-scale=1.0">
+    <title>Order</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="Project_1.php">Inventory Search</a>
+            <a class="navbar-brand" href="Project_2.php">Order</a>
+            <a class="navbar-brand" href="Project_3.php">Order Summary</a>
+        </nav>
+
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get">
+            <div class="form-group">
+                <label for="product_id">Product_ID</label>
+                <input type="text" name="productId" class="form-control" placeholder="Enter Product_ID ex) 10001">
+            </div>
+            <div class="form-group">
+                <label for="customer_id">Customer_ID</label>
+                <input type="text" name="customerId" class="form-control" placeholder="Enter Customer_ID ex) 1">
+            </div>
+            <div class="form-group">
+                <label for="Quantity">Quantity</label>
+                <input type="text" name="quantity" class="form-control" placeholder="Enter Quantity">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <?php 
+            $productId = $customerId = $quantity = $orderPrice = $result = "";
+
+            if (isset($_GET['productId']) && isset($_GET['customerId']) && isset($_GET['quantity'])) {
+
+                if (empty($_GET['productId'])) {
+                    echo 'Product_Id is required<br>';
+                } else {
+                    $productId = intval($_GET["productId"]);
+                    echo "Success: productID ".$productId."<br>";
+                }
+
+                if (empty($_GET['customerId'])) {
+                    echo 'Customer_Id is required<br>';
+                } else {
+                    $customerId = intval($_GET["customerId"]);
+                    echo "Success: customerId ".$customerId."<br>";
+                }
+
+                if (empty($_GET['quantity'])) {
+                    echo 'Quantity is required<br>';
+                } else {
+                    $quantity = intval($_GET['quantity']);
+                    echo "Success: quantity ".$quantity."<br>";
+                }
+            }
+            
+            //query the data by productId to find the Order_Price (List_Price*Quantity)
+            $sql = "SELECT List_Price FROM Product WHERE Product_ID = ".$productId;
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "List_Price: ".$row["List_Price"]."<br>";
+                    $orderPrice = $row["List_Price"]*$quantity;
+                    echo "Order_Price: ".$orderPrice."<br>";
+
+                    $sql = "INSERT INTO Order_Customers (Product_ID, Customer_ID, Quantity, Order_Price)
+                    VALUES ($productId, $customerId , $quantity, $orderPrice)";
+
+                    if (mysqli_query($conn, $sql)) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                }
+            } else {
+                echo "0 results<br>";
+            }
+        ?>
+        <br>
+    </div>
+</body>
+
+</html>
